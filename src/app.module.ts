c74fb9e392f1d08e16e8e20211dc512bbea8d738
Deepanshu <deepanshu@rapidinnovation.dev>
2022-10-20 18:42:45 +0530

    Learn Graphql and setup mongo

---
+++ src/app.module.ts
@@ -0,0 +1,17 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { LessonModule } from './lesson/lesson.module';
+import { LessonResolver } from './lesson/lesson.resolver';
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      autoSchemaFile: true,
+      driver: ApolloDriver,
+    }),
+    LessonModule,
+  ],
+  providers: [LessonResolver],
+})
+export class AppModule {}
